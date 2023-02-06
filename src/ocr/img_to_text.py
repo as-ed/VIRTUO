b3381cdacea8e147cd675829920f4dbcbc917a2c
Matthew Davidson <matthewd0224@gmail.com>
2023-02-06 16:15:00 +0000

    implemented tesseract ocr in img_to_text.py

--- src/ocr/img_to_text.py
+++ src/ocr/img_to_text.py
@@ -1,9 +1,11 @@
 import numpy as np
-
+from PIL import Image
+import pytesseract
 
 def get_text(img: np.ndarray) -> str:
-	pass
-
+    PILImage = Image.fromarray(img)
+    text = pytesseract.image_to_string(PILImage)
+    return text
 
 def post_processing(text: str) -> str:
 	return text
