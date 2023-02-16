99dca53006efd5f3d4c2b9a40aeb88a753dc880b
sammygb26 <s2097950@ed.ac.uk>
2023-02-16 12:46:26 +0000

    Updated main.py so that turn_page has parameters (used for testing)

--- src/Hardware/demo/main.py
+++ src/Hardware/demo/main.py
@@ -10,22 +10,21 @@ m = Motor(mc, [2], 0, [-80], [80])
 s = Motor(mc, [1, 3], 1, [60, 60], [-60, -60])
 
 
-def turn_page():
+def turn_page(slider_down_time = 0.35, m_angles = [0, -6, -12]):
     print("start up")
-    m.set_angle(0)
-    s.set_angle(0)
+    m.set_angle(m_angles[0])
 
     print("setting up arm")
-    m.move_to_angle(-8)
-    s.move_forward(0.35)
+    m.move_to_angle(m_angles[1])
+    s.move_forward(slider_down_time)
     f.on()
    
     print("picking up page")
-    m.move_to_angle(-18)
+    m.move_to_angle(m_angles[2])
     time.sleep(1)
 
     print("resetting")
-    m.move_to_angle(0)
+    m.move_to_angle(m_angles[0])
     s.move_backward(0.8)
     f.off()
 
