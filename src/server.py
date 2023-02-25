891c3a38bdecd53d4ddb2cbe5ae8916ecddff626
as-ed <s2106887@ed.ac.uk>
2023-02-25 14:43:34 +0000

    revert some changes and minor fixes

--- src/server.py
+++ src/server.py
@@ -1,122 +1,119 @@
-from flask import Flask, redirect, render_template, request, send_file
-from markupsafe import escape
-#from werkzeug.middleware.proxy_fix import ProxyFix
 import os
-import controller
-import file_converter
+from typing import Union, Tuple
 
-app = Flask(__name__)
+from flask import Flask, redirect, render_template, request, send_from_directory, Response
 
-#app.wsgi_app = ProxyFix(
-#            app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-#        )
+from config import CFG
+import controller as cont
+import file_converter as conv
 
-@app.route("/")
-def home():
-    
-    '''
-    
-    book = {
-            title: date or user chosen date
-            author: author of book
-            creation-time: date + time of creation of book
-            }
 
-    books/
-        20230210-1715/
-            20230210-1715.txt
-            metadata.json
-    '''
+server = Flask(__name__)
 
-    if os.path.exists("books/"):
-        dirs = [name for name in os.listdir("books")]
-    else:
-        dirs = []
-    voices = controller.get_voices()
-    currVoice = controller.get_current_voice()
-    voices.remove(currVoice)
-    return render_template("home.html", books=dirs, voices=voices, currVoice=currVoice)
 
-@app.route('/books/<filename>/book.txt')
-def download_txt(filename):
-    if not os.path.isfile('books/' + filename + '/book.txt'):
-        return render_template('redirect.html')
-    else:
-        return send_file('./books/' + filename + '/book.txt')
+@server.get("/favicon.ico")
+def favicon() -> Response:
+	return send_from_directory(os.path.join(server.root_path, "static"), "favicon.ico")
 
-@app.route('/books/<filename>/book.pdf')
-def download_pdf(filename):
-    file_converter.create_pdf('books/' + filename)
-    return send_file('./books/' + filename + '/book.pdf')
 
-@app.route('/books/<filename>/book.epub')
-def download_epub(filename):
-    file_converter.create_epub('books/' + filename)
-    return send_file('./books/' + filename + '/book.epub')
+@server.get("/")
+def home() -> str:
+	voices = cont.get_voices()
+	current_voice = cont.get_current_voice()
+	voices.remove(current_voice)
+	return render_template("home.html", books=os.listdir(CFG["book_location"]), voices=voices, currVoice=current_voice)
 
-@app.route('/books/<filename>/book.mp3')
-def download_mp3(filename):
-    file_converter.create_mp3('books/' + filename)
-    return send_file('./books/' + filename + '/book.mp3')
 
-@app.route('/system/scan/start', methods=['GET','POST'])
-def begin_scan():
-    if request.method == 'POST':
-        scan = controller.scan()
-        return str(scan), 204
-    else:
-        return redirect('/', 307)
+@server.get("/books/<book>/book.txt")
+def download_txt(book) -> Union[str, Response]:
+	if not os.path.isfile(_get_path(book, "book.txt")):
+		return render_template("redirect.html")
+	else:
+		return send_from_directory(_get_path(book), "book.txt")
 
-@app.route('/system/getVolume', methods=['GET'])
-def get_volume():
-   volume = 20 
-   return str(volume)
 
-@app.route('/system/setVolume/<val>', methods=['GET', 'POST'])
-def set_volume(val):
-    if request.method == 'POST':
-        volume = int(val) / 100
-        return str(controller.set_volume(volume))
-    else:
-        return redirect('/', 307)
+@server.get("/books/<book>/book.pdf")
+def download_pdf(book) -> Response:
+	conv.create_pdf(_get_path(book))
+	return send_from_directory(_get_path(book), "book.pdf")
 
-@app.route('/system/setVoice/<val>', methods=['GET', 'POST'])
-def set_voice(val):
-    if request.method == 'POST':
-        if val in controller.get_voices():
-            return str(controller.set_voice(val))
-        else:
-            return ''
-    else:
-        return redirect('/', 307)
 
-@app.route('/system/scan/stop', methods=['GET', 'POST'])
-def stop_scan():
-    if request.method == 'POST':
-        return str(controller.stop_scan())
-    else:
-        return redirect('/', 307)
+@server.route("/books/<book>/book.epub")
+def download_epub(book) -> Response:
+	conv.create_epub(_get_path(book))
+	return send_from_directory(_get_path(book), "book.epub")
 
-@app.route('/system/togglePause', methods=['GET', 'POST'])
-def toggle_pause():
-    if request.method == 'POST':
-        response = str(controller.toggle_pause())
-        return response
-    else:
-        return redirect('/', 307)
 
-@app.route('/system/fast-forward', methods=['GET', 'POST'])
-def fast_forward():
-    if request.method == 'POST':
-        controller.fast_forward()
-        return ''
-    else:
-        return redirect('/', 307)
+@server.route("/books/<book>/book.mp3")
+def download_mp3(book) -> Response:
+	conv.create_mp3(_get_path(book))
+	return send_from_directory(_get_path(book), "/book.mp3")
 
-@app.route('/system/rewind', methods=['GET', 'POST'])
-def rewind():
-    if request.method == 'POST':
-        controller.rewind()
-        return ''
-    else:
-        return redirect('/', 307)
+
+@server.route("/system/scan/start", methods=["GET", "POST"])
+def begin_scan() -> Union[str, Response]:
+	if request.method == "POST":
+		scanning = cont.scan()
+		return str(scanning)
+	else:
+		return redirect("/", 307)
+
+
+@server.route("/system/scan/stop", methods=["GET", "POST"])
+def stop_scan() -> Union[Tuple[str, int], Response]:
+	if request.method == "POST":
+		cont.stop_scan()
+		return "", 204
+	else:
+		return redirect("/", 307)
+
+
+@server.get("/system/getVolume")
+def get_volume() -> str:
+	return str(cont.get_volume())
+
+
+@server.route("/system/setVolume/<value>", methods=["GET", "POST"])
+def set_volume(value) -> Union[str, Response]:
+	if request.method == "POST":
+		return str(cont.set_volume(int(value) / 100))
+	else:
+		return redirect("/", 307)
+
+
+@server.route("/system/setVoice/<value>", methods=["GET", "POST"])
+def set_voice(value) -> Union[str, Response]:
+	if request.method == "POST":
+		return str(cont.set_voice(value))
+	else:
+		return redirect("/", 307)
+
+
+@server.route("/system/togglePause", methods=["GET", "POST"])
+def toggle_pause() -> Union[str, Response]:
+	if request.method == "POST":
+		return str(cont.toggle_pause())
+	else:
+		return redirect("/", 307)
+
+
+@server.route("/system/fastForward", methods=["GET", "POST"])
+def fast_forward() -> Union[str, Response]:
+	if request.method == "POST":
+		cont.fast_forward()
+		return ""
+	else:
+		return redirect("/", 307)
+
+
+@server.route("/system/rewind", methods=["GET", "POST"])
+def rewind() -> Union[str, Response]:
+	if request.method == "POST":
+		cont.rewind()
+		return ""
+	else:
+		return redirect("/", 307)
+
+
+def _get_path(book: str, file: str = None) -> str:
+	return os.path.join(CFG["book_location"], book) if file is None else os.path.join(CFG["book_location"], book, file)
--- src/static/script.js
+++ src/static/script.js
@@ -1,42 +1,40 @@
 function startScan() {
-  fetch('system/scan/start', {method: 'POST'})
+	fetch('system/scan/start', {method: 'POST'})
 }
 
 function stopScan() {
-  fetch('system/scan/stop', {method: 'POST'})
+	fetch('system/scan/stop', {method: 'POST'})
 }
 
 function getVolume(){
-  fetch(
-    '/system/getVolume', {method:'GET'})
-    .then((response) => response.text())
-    .then((text) => {
-      document.getElementById("volume").value = text
-    })
+	fetch(
+		'/system/getVolume', {method:'GET'})
+		.then((response) => response.text())
+		.then((text) => {
+			document.getElementById("volume").value = text * 100
+		})
 }
 
 function setVolume(){
-  fetch('/system/setVolume/' + document.getElementById("volume").value, {method: 'POST'})
+	fetch('/system/setVolume/' + document.getElementById("volume").value, {method: 'POST'})
 }
 
 function togglePause(){
-  fetch('/system/togglePause', {method: 'POST'})
-  .then((response) => response.text())
-  .then((text) => {
-    let playpause = (text === "False") ? "Play":"Pause";
-    document.getElementById("playpause").innerHTML = playpause
-  })
+	fetch('/system/togglePause', {method: 'POST'})
+		.then((response) => response.text())
+		.then((text) => {
+			document.getElementById("playpause").innerHTML = (text === "False") ? "Play" : "Pause"
+		})
 }
 
 function setVoice(){
-  fetch('/system/setVoice/' + document.getElementById("voiceSelect").value, {method: 'POST'})
+	fetch('/system/setVoice/' + document.getElementById("voiceSelect").value, {method: 'POST'})
 }
 
-function fastforward() {
-  fetch('system/fast-forward', {method: 'POST'})
+function fastForward() {
+	fetch('system/fastForward', {method: 'POST'})
 }
 
 function rewind() {
-  fetch('system/rewind', {method: 'POST'})
+	fetch('system/rewind', {method: 'POST'})
 }
-
