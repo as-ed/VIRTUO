3239e58d15a782c589c3f43b26c9766065fb5332
sammygb26 <s2097950@ed.ac.uk>
2023-03-09 11:48:49 +0000

    update

--- src/Hardware/demo2/main.py
+++ src/Hardware/demo2/main.py
@@ -1,15 +1,47 @@
-from motors import Motors
+import motors
+
 from devices import *
 from gpioi import *
+from util import *
 
 import time
 
-mc = Motors()
-f = FanGroup(mc, [])
+mc = motors.Motors()
 
-m_start_sensor = Button(8)
-m = Motor(mc, [0], forward_values=[-80], start_sensor=m_start_sensor)
+main_motor = Motor(mc, [0], None, [60], Button(8))
+slider = Motor(mc, [4,5], None, [60,60], None)
+fans = FanGroup(mc, [1,2,3])
 
+def setup_main_motor():
+    global main_motor
+    main_motor = setup_motor()
+
+def setup_slider():
+    global slider
+    slider = setup_motor()
+
+
+def setup_fans():
+    global fans
+
+    pins = []
+    while read_y_n("Add another fan pin? (y/n)"):
+        pins += read_safe_int("Pin:")
+
+    fans = FanGroup(mc, pins)
+
+def setup():
+    if read_y_n("Set up main motor? (y/n)"):
+        setup_main_motor()
+
+    if read_y_n("Set up slider? (y/n)"):
+        setup_slider()
+
+    if read_y_n("Set up fans? (y/n)"):
+        setup_fans()
+
+def turn_page():
+    m.reset()
 
 def main():
     pass
