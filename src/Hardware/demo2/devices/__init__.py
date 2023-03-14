a8e7f3683770b17621c876cf3411936b56ed450b
sammygb26 <s2097950@ed.ac.uk>
2023-03-14 17:55:05 +0000

    added

--- src/Hardware/demo2/devices/__init__.py
+++ src/Hardware/demo2/devices/__init__.py
@@ -3,4 +3,5 @@ from devices.mainMotor import MainMotor
 from devices.fanGroup import FanGroup
 from devices.motorPin import MotorPin
 from devices.slider import Slider
+from devices.baseClipper import BaseClipper
 
---
+++ src/Hardware/demo2/devices/baseClipper.py
@@ -0,0 +1,24 @@
+from devices.motorPin import MotorPin
+import time
+
+class BaseClipper:
+    def __init__(self, rotator, extender, rotator_forward, extender_forward, extend_time=0.2):
+        self.rotator = rotator
+        self.rotator_forward = rotator_forward
+
+        self.extender = extender
+        self.extender_forward = extender_forward
+        self.extend_time = extend_time
+
+    def extend(self):
+        self.extender.move(self.extender_forward)
+        time.sleep(self.extend_time)
+        self.extender.stop()
+
+    def retract(self):
+        self.extender.move(-self.extender_forward)
+        time.sleep(self.extend_time)
+        self.extender.stop()
+
+
+
