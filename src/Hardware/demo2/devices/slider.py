81bc4410c8cb9236ac03e216a53882503f49b2d3
sammygb26 <s2097950@ed.ac.uk>
2023-03-20 09:05:30 +0000

    Changed movement values to be more reasonable

--- src/Hardware/demo2/devices/slider.py
+++ src/Hardware/demo2/devices/slider.py
@@ -2,7 +2,7 @@ from devices.motorPin import MotorPin
 import time
 
 class Slider:
-    def __init__(self, pins, up=60, down=-60):
+    def __init__(self, pins, up=50, down=-50):
         self.pins = pins
         self.up_speed = up
         self.down_speed = down
